b82e89d9ba65f97848ad8c6ae3ead0575c9965da
Raja <eromurugan@gmail.com>
2018-08-29 01:21:24 +0530

    Added fontawesome for down icons and attached event to it.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  onShowClick = e => {
+    console.log(e.target);
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>
+          {name} <i className="fas fa-sort-down" onClick={this.onShowClick} />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
