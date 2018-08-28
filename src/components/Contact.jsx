83bc724306d311ef60de0c8a7cd07014030bd457
Raja <eromurugan@gmail.com>
2018-08-29 01:32:48 +0530

    Update the component state within the component.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,20 +2,29 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
-  onShowClick = e => {};
+  state = { showContactInfo: false };
+
+  onShowClick = e => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
         <h4>
           {name} <i className="fas fa-sort-down" onClick={this.onShowClick} />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
