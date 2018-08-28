561e6afad841f9866ccdeb1fa25ba4c7c691e538
Raja <eromurugan@gmail.com>
2018-08-29 01:07:31 +0530

    Added Bootstrap Styling.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Header = props => {
-  const headerStyle = { color: "red", fontSize: "40px" };
+  const headerStyle = "navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0";
 
   return (
-    <div>
-      <h1 style={headerStyle}>{props.branding}</h1>
-    </div>
+    <nav className={headerStyle}>
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {props.branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
