60f11e423b456f37ac25a70dd7855aa14c3a137d
Raja <eromurugan@gmail.com>
2018-08-29 01:10:30 +0530

    Add Contacts container component. with state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,16 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Contact
-          name="Raja Singaram"
-          email="eromurugan@gmail.com"
-          phone="555-555-5555"
-        />
-        <Contact
-          name="Priya Singaram"
-          email="eropriya@gmail.com"
-          phone="666-666-6666"
-        />
+        <div className="conatiner">
+          <Contacts />
+        </div>
       </div>
     );
   }
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+class Contacts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [
+        {
+          id: 1,
+          name: "Raja Singaram",
+          email: "eromurugan@gmail.com",
+          phone: "555-555-5555"
+        },
+        {
+          id: 2,
+          name: "Priya Singaram",
+          email: "eropriya@gmail.com",
+          phone: "666-666-6666"
+        },
+        {
+          id: 3,
+          name: "Selvi Singaram",
+          email: "eroselvi@gmail.com",
+          phone: "777-777-7777"
+        }
+      ]
+    };
+  }
+
+  render() {
+    const { contacts } = this.state;
+
+    const mapContact = contact => (
+      <Contact
+        key={contact.id}
+        name={contact.name}
+        email={contact.email}
+        phone={contact.phone}
+      />
+    );
+
+    return <div>{contacts.map(mapContact)}</div>;
+  }
+}
+
+export default Contacts;
