db6323768253904699fdebe8fb363a6229e57669
Raja <eromurugan@gmail.com>
2018-08-29 02:22:14 +0530

    Added the reducer and action to delete contact in the Context.

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,14 +3,6 @@ import Contact from "./Contact";
 import { Consumer } from "../context";
 
 class Contacts extends Component {
-  deleteContact = id => {
-    const { contacts } = this.state;
-
-    const newContacts = contacts.filter(contact => contact.id !== id);
-
-    this.setState({ contacts: newContacts });
-  };
-
   render() {
     return (
       <Consumer>
@@ -18,11 +10,7 @@ class Contacts extends Component {
           const { contacts } = value;
 
           const mapContact = contact => (
-            <Contact
-              key={contact.id}
-              contact={contact}
-              deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-            />
+            <Contact key={contact.id} contact={contact} />
           );
 
           return <React.Fragment>{contacts.map(mapContact)}</React.Fragment>;
