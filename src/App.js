dfd5dd1b000b308263d048d45be607745cc13fc2
Raja <eromurugan@gmail.com>
2018-09-09 00:26:33 +0530

    Lifecycle Events

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contacts from "./components/contacts/Contacts";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
 import Header from "./components/layout/Header";
+import Test from "./components/test/Test";
 import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
               </Switch>
             </div>
