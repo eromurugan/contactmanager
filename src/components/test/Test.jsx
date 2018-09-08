513f1e6e49473ad677f6cbb985fcf58d6c54eef9
Raja <eromurugan@gmail.com>
2018-09-09 00:32:14 +0530

    Add JSON Call to fetch data using node default "fetch" library

--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 
 class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
   componentDidMount() {
     console.log("Component Did Mount...");
+
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
   }
 
   componentWillMount() {
@@ -25,9 +39,11 @@ class Test extends Component {
   }
 
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
