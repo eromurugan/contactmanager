54c470d1598bc4e62a483bb342ef9015337f34f6
Raja <eromurugan@gmail.com>
2018-09-09 00:35:17 +0530

    Added Axios Library to make AJAX Calls

--- src/context.js
+++ src/context.js
@@ -25,28 +25,11 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: "Raja Singaram",
-        email: "eromurugan@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Priya Singaram",
-        email: "eropriya@gmail.com",
-        phone: "666-666-6666"
-      },
-      {
-        id: 3,
-        name: "Selvi Singaram",
-        email: "eroselvi@gmail.com",
-        phone: "777-777-7777"
-      }
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  componentDidMount() {}
 
   render() {
     return (
