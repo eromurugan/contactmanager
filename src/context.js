87b521ed82000300dd3066e7056954944701b435
Raja <eromurugan@gmail.com>
2018-09-09 00:39:41 +0530

    Added GET Call to fetch Contacts from JSONPlaceholder.typicode.com

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
@@ -29,7 +30,13 @@ export class Provider extends Component {
     dispatch: action => this.setState(state => reducer(state, action))
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(res =>
+      this.setState({
+        contacts: res.data
+      })
+    );
+  }
 
   render() {
     return (
