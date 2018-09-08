c18fba7abb18b534dbd8dc2ebfdbdb2af805150f
Raja <eromurugan@gmail.com>
2018-09-08 23:19:32 +0530

    Default routing. Added the Not Found Page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddContact from "./components/contacts/AddContact";
 import Contacts from "./components/contacts/Contacts";
 import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import Header from "./components/layout/Header";
 import { Provider } from "./context";
 
@@ -21,6 +22,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
